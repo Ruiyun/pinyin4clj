daebd100c6dd45c65a07c79730114d00994658bd
Ruiyun <ruiyun.wen@gmail.com>
2013-05-11 15:30:54 +0800

    back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pinyin4clj "0.2.0"
+(defproject pinyin4clj "0.3.0-SNAPSHOT"
   :description "获取汉字拼音的Clojure库，对pinyin4j进行了简单封装。"
   :url "http://github.com/Ruiyun/pinyin4clj"
   :license {:name "Eclipse Public License"
