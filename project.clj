18d2eab35941473a30405ad70bd3ff8b8fa59917
Ruiyun <ruiyun.wen@gmail.com>
2013-07-31 16:14:40 +0800

    change dependence to ruiyun/pinyin4j

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject pinyin4clj "0.3.0-SNAPSHOT"
+(defproject pinyin4clj "0.3.0"
   :description "获取汉字拼音的Clojure库，对pinyin4j进行了简单封装。"
   :url "http://github.com/Ruiyun/pinyin4clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.belerweb/pinyin4j "2.5.0"]]
+  :dependencies [[ruiyun/pinyin4j "2.5.0"]]
   :plugins [[codox "0.6.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
